c284b181d3cb8681c6bfbd73af8a8091048e0040
chanpreet <chanpreet.chhabra@gmail.com>
2017-01-23 00:38:30 +0530

    Genre search

--- app/src/main/java/chanpreet/searcher/Activity/MainActivity.java
+++ app/src/main/java/chanpreet/searcher/Activity/MainActivity.java
@@ -1,10 +1,13 @@
 package chanpreet.searcher.Activity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
@@ -14,9 +17,11 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -38,30 +43,68 @@ public class MainActivity extends AppCompatActivity {
     private SearchView searchView;
     private MenuItem menuItem;
     private SearchView.OnQueryTextListener listener;
+    private TextView initialText;
     Search search = new Search();
     ProgressDialog pd;
+    private CoordinatorLayout cLayout;
     private ArrayList<SearchResponse> list = new ArrayList<>();
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private LinearLayoutManager linearLayoutManager;
+    private String genre[];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        cLayout = (CoordinatorLayout) findViewById(R.id.c_layout);
+        initialText = (TextView) findViewById(R.id.initial_text);
         pd = Tools.getProgressDialog(MainActivity.this);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Select a genre")
+                        .setItems(R.array.genre_array, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                genre = getResources().getStringArray(R.array.genre_array);
+                                search.setKey(genre[which]);
+                                pd.show();
+                                ApiInterface apiService = ApiClient.getRetrofitClient().create(ApiInterface.class);
+                                Call<Resp> call = apiService.Searched(search);
+                                call.enqueue(new Callback<Resp>() {
+                                    @Override
+                                    public void onResponse(Call<Resp> call, Response<Resp> response) {
+                                        if (response.isSuccessful()) {
+                                            initialText.setVisibility(View.GONE);
+                                            pd.dismiss();
+                                            list = response.body().getList();
+                                            Snackbar snackbar = Snackbar
+                                                    .make(cLayout, "Fetching Data", Snackbar.LENGTH_SHORT);
+                                            snackbar.show();
+                                            setRecyclerView();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onFailure(Call<Resp> call, Throwable t) {
+                                        pd.dismiss();
+                                        Snackbar snackbar = Snackbar
+                                                .make(cLayout, "Can't connect!! Check your Internet Connection", Snackbar.LENGTH_LONG);
+                                        snackbar.show();
+                                    }
+                                });
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
 
-         listener = new SearchView.OnQueryTextListener() {
+        listener = new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 search.setKey(query);
@@ -71,11 +114,13 @@ public class MainActivity extends AppCompatActivity {
                 call.enqueue(new Callback<Resp>() {
                     @Override
                     public void onResponse(Call<Resp> call, Response<Resp> response) {
-                        if(response.isSuccessful())
-                        {
+                        if (response.isSuccessful()) {
+                            initialText.setVisibility(View.GONE);
                             pd.dismiss();
                             list = response.body().getList();
-                            Toast.makeText(MainActivity.this,"Successful",Toast.LENGTH_SHORT).show();
+                            Snackbar snackbar = Snackbar
+                                    .make(cLayout, "Fetching Data", Snackbar.LENGTH_SHORT);
+                            snackbar.show();
                             setRecyclerView();
                         }
                     }
@@ -83,8 +128,9 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onFailure(Call<Resp> call, Throwable t) {
                         pd.dismiss();
-                        Log.e("error",t.getCause().toString());
-                        Toast.makeText(MainActivity.this,"UNSuccessful",Toast.LENGTH_SHORT).show();
+                        Snackbar snackbar = Snackbar
+                                .make(cLayout, "Can't connect!! Check your Internet Connection", Snackbar.LENGTH_LONG);
+                        snackbar.show();
                     }
                 });
                 return true;
@@ -99,17 +145,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void setRecyclerView() {
         recyclerView = (RecyclerView) findViewById(R.id.movies_recycler);
-        adapter = new MovieAdapter(MainActivity.this,list);
+        adapter = new MovieAdapter(MainActivity.this, list);
         linearLayoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(linearLayoutManager);
     }
 
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -118,16 +159,5 @@ public class MainActivity extends AppCompatActivity {
         searchView = (SearchView) menuItem.getActionView();
         searchView.setOnQueryTextListener(listener);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
